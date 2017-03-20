f2ec3fdc0275cd358b6f354bdb88fd92e1ee01d1
Patrick Steiner <patrick@steiner-buchholz.de>
2017-03-20 20:21:49 +0000

    inital data for JBoss BRMS

---
+++ DataSet/src/main/java/com/redhat/demo/iot/utils/App.java
@@ -0,0 +1,13 @@
+package com.redhat.demo.iot.utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
